e33989e2152399899c1adb15c5aa59b2e0eb865e
Danny Brito <britodanny1997@gmail.com>
2019-10-01 02:25:54 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,7 +12,7 @@ songs = [
 # Add your code here
 
 def help
-  puts "I accept the following commands"
+  puts "I accept the following commands:"
   puts "- help : display this help message"
   puts "- list : display a list of songs you can play"
   puts "- play : lets you choose a song to play"
