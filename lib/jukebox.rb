420488bc7e97b9663ee516b4db6107a4b2a5a958
Danny Brito <britodanny1997@gmail.com>
2019-10-01 02:46:23 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,7 @@ def play(songs)
   song_input = gets.strip
   if song_input.length == 1
     song_input.to_i 
-    puts "Playing #{songs[song_input-1]}"if song_input >=1 &&song_input <=9
+    puts "Playing #{songs[song_input-1]}"if song_input >=1 && song_input <=9
     return
   else
     songs.each{|ind_song|
